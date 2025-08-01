910cc3ce88d61ac6a52bcc6aa355bc4baa6450ac
JeevanReddy0828 <jeevanr.arlagadda@gmail.com>
2025-08-01 12:40:31 -0500

    Update processor.py

--- processor.py
+++ processor.py
@@ -20,7 +20,7 @@ def clean_up_sentence(sentence):
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+# returning bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
 def bow(sentence, words, show_details=True):
     # tokenize the pattern
@@ -30,19 +30,19 @@ def bow(sentence, words, show_details=True):
     for s in sentence_words:
         for i,w in enumerate(words):
             if w == s:
-                # assign 1 if current word is in the vocabulary position
+                # assigning 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
     return(np.array(bag))
 
 def predict_class(sentence, model):
-    # filter out predictions below a threshold
+    # filtering out predictions below a threshold
     p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
+    # sorted by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
@@ -61,6 +61,11 @@ def getResponse(ints, intents_json):
     return result
 
 def chatbot_response(msg):
+    # Checking if it's a training-related query
+    if "training" in msg.lower() or "plan" in msg.lower():
+        return generate_training_plan(msg)
+
     ints = predict_class(msg, model)
     res = getResponse(ints, intents)
     return res
+
